a9b47d45903eae8ac9e22ef0c632a4978e4b3e29
cristianorregoidico <cristianorrego@idico.com>
2024-07-05 10:40:47 -0500

    feat: agrega funcion para realizar la suma de 2 numeros

---
+++ src/utils/add.ts
@@ -0,0 +1,13 @@
+/**
+ * Calculate the sum of two numbers
+ * @param {number} a - First number to add 
+ * @param {number} b - Second number to add 
+ * @returns {number} The sum of the two numbers
+ */
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+
+export default add
